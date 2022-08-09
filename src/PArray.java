a578324e37ee565581a16f61f6dc51df8c67e391
rashmitha66 <sai.rashmitha2001@gmail.com>
2022-08-09 11:39:47 +0530

    Day_2 PracticeArray Commit

--- src/PArray.java
+++ src/PArray.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class PArray {
+import java.util.*;
+public class PArray {
+    public static void main (String args[]){
+        Scanner sc=new Scanner(System.in);
+        //int n=sc.nextInt();
+//        int m=sc .nextInt();
+//        int a[][] = new int[n][m];
+        int n=10;
+        int a[]= new int [10];
+        for(int i=0;i<10;i++){
+            a[i]=sc.nextInt();
+        }
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        int max_index=0;
+        int min_index=0;
+        for(int i=0;i<10;i++){
+            if(a[i]>max){
+                max=a[i];
+                max_index=i;
+            }
+            if(a[i]<min){
+                min=a[i];
+                min_index=i;
+            }
+        }
+        System.out.println(min);
+        System.out.println(max);
+
+    }
 }
