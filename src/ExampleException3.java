1c2c05c326f66659718fb7f09564367d70260699
rashmitha66 <sai.rashmitha2001@gmail.com>
2022-08-10 15:37:28 +0530

    Day_3 Exception_Handling Null_pointer Commit

---
+++ src/ExampleException3.java
@@ -0,0 +1,13 @@
+public class ExampleException3 {
+    public static void main(String args[]){
+        String s="";
+        try{
+            System.out.println(s.length());
+
+        }catch(Exception e){
+            System.out.println(e);
+        };
+
+        System.out.print("continuing with rest of the code");
+    }
+}
